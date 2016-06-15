428d0a8037759f806edcad3caeac65d33ab282af
Boniface Mwenda <bonny90.m@gmail.com>
2016-06-15 23:58:58 +0300

    tests for fizzbuzz

---
+++ test_fizzbuzz.py
@@ -0,0 +1,24 @@
+import unittest
+import fizzbuzz
+#from fizzbuzz import fizz_buzz
+
+class FizzBuzzTest(unittest.TestCase):
+	""" Testing fizzbuzz
+	"""
+	def test_returns_fizz(self):
+		"""Returns fizz if the input is
+			divisible by three
+		"""
+		self.assertEqual(fizzbuzz.fizz_buzz(3),'fizz')
+
+	def test_returns_buzz(self):
+		"""Returns buzz if the input is
+		divisible by five
+		"""
+		self.assertEqual(fizzbuzz.fizz_buzz(5),'buzz')
+
+	def test_returns_fizzbuzz(self):
+		"""Returns fizzbuzz if the input is
+		divisible by fifteen
+		"""
+		self.assertEqual(fizzbuzz.fizz_buzz(15),'fizzbuzz')
