0ab9c57049272e0e16d6d562417a4031192d932b
Boniface Mwenda <bonny90.m@gmail.com>
2016-06-16 00:07:21 +0300

    list of n prime numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,22 +1,23 @@
+#Function to get the 1st n prime numbers
+def get_prime(x):
+	lst=[]
+	j=0
+	while len(lst)<x:
+		if check_prime(j)==True:
+			lst.append(j)
+		j+=1
+
+	print(lst)
+
 #A function to check if a number is prime
-def checkPrime(n):
+def check_prime(n):
 	if n<2:
 		return False
 	for i in range(2,int(n**0.5)+1):
 		if n%i==0:
 			return False
 	return True
-
-#Get the 1st n prime numbers
-def getPrime(x):
-	lst=[]
-	j=0
-	while len(lst)<x:
-		if checkPrime(j)==True:
-			lst.append(j)
-		j+=1
-
-	print(lst)
-getPrime(5)
+#Get a list of the first 20 prime numbers:
+get_prime(20)
 
 
