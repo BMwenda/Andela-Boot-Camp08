30ccc7ee6e5610c322ad5c68ce60de7491e4f859
Boniface Mwenda <bonny90.m@gmail.com>
2016-06-19 19:28:40 +0300

    Fizz buzz function

---
+++ Fizzbuzz/fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizz_buzz(n):
+	"""Return fizz when divisible by 3
+		return buzz when divisible by 5
+		return fizzbuzz when divisible by both 3 and 5
+	"""
+
+	if n%15==0:
+		return 'fizzbuzz'
+	elif n%3==0:
+		return 'fizz'
+	elif n%5==0:
+		return 'buzz'
+	else:
+		pass
