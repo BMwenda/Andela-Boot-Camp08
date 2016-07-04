dc348ba35a158337e764e8a50154aadb66831f86
Boniface Mwenda <bonny90.m@gmail.com>
2016-07-04 15:42:30 +0300

    Added error handling functions

--- Fizzbuzz/fizzbuzz.py
+++ Fizzbuzz/fizzbuzz.py
@@ -1,14 +1,29 @@
 def fizz_buzz(n):
-	"""Return fizz when divisible by 3
-		return buzz when divisible by 5
-		return fizzbuzz when divisible by both 3 and 5
-	"""
+    """Return fizz when divisible by 3 \
+    return buzz when divisible by 5 \
+    return fizzbuzz when divisible by both 3 and 5"""
 
-	if n%15==0:
-		return 'fizzbuzz'
-	elif n%3==0:
-		return 'fizz'
-	elif n%5==0:
-		return 'buzz'
-	else:
-		pass
+    # Check for null values
+    if n == '':
+        raise Exception("No argument passed")
+
+    # Confirm that n is an integer
+    if type(n) is not int:
+        raise TypeError("Only integers are allowed")
+
+    # Raise error if number is negative
+    if n < 0:
+        raise Exception("Only positive integers allowed")
+
+    # Check for zero, since 0 % n is always 0.
+    if n == 0:
+        return 0
+
+    if n % 15 == 0:
+        return 'fizzbuzz'
+    elif n % 3 == 0:
+        return 'fizz'
+    elif n % 5 == 0:
+        return 'buzz'
+    else:
+        return n
